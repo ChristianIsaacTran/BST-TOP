912ddc46659ca58e4446f47a555ba75d34ed56ac
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-07-24 02:47:46 -0500

    Added a node.js and tree.js respectively
    node.js contains the factory to make
    node instances to be used in the binary search tree
    data structure, and the tree.js contains the
    logic for the actual binary search tree.

---
+++ src/factories/node.js
@@ -0,0 +1,7 @@
+
+export default function node(inputValue, inputLeftNode, inputRightNode) {
+    const nodeValue = inputValue;
+    const nodeLeft = inputLeftNode;
+    const nodeRight = inputRightNode;
+    return { nodeValue, nodeLeft, nodeRight };
+}
---
+++ src/factories/tree.js
@@ -0,0 +1,15 @@
+
+
+export default binaryTree() {
+    let root;
+
+    /*
+    takes in an array of data and turns it into a balanced binary tree full of nodes.
+    It also says to explicitly sort the array and remove any duplicate values to ensure tree building is as even as possible.
+    */
+    function buildTree(inputArr) {
+
+    }
+
+    return { root, buildTree };
+}
